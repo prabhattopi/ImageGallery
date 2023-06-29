e7ccc05257cc15804d3f226518e115f83b8a2de7
prabhattopi <topi9864@gmail.com>
2023-06-29 19:23:46 +0530

    the file di dhfsd

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { FirebaseApp, initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
