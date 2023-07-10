85014aa5549fb47ce4eeb5ed606815622c4d79b7
prabhattopi <topi9864@gmail.com>
2023-07-10 21:12:07 +0530

    the file is need ot update with form data

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import Signup from "./pages/Signup"
 import PublicRoutes from "./routes/PublicRoutes"
 import PrivateRoutes from "./routes/PrivateRoutes"
+import ImageName from "./components/ImageName"
 import { useState } from "react"
 import { useCopyToClipboard } from "usehooks-ts"
 import { saveAs } from "file-saver"
@@ -10,6 +11,7 @@ function App() {
   const [showToast, setShowToast] = useState(false)
   const [toastMessage, setToastMessage] = useState<string>("")
   const [, copy] = useCopyToClipboard()
+  const [modal,setModal]=useState(true)
   const handleClick = async (text: string, order: string) => {
     if (order == "copy") {
       copy(text)
@@ -41,6 +43,27 @@ function App() {
 
   return (
     <>
+      <div className={`fade-overlay ${modal ? "fade-overlay-visible" : ""}`} />
+      {modal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-200">
+          <div className="">
+            <input
+              type="text"
+              placeholder="Enter your text"
+              className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
+            />
+            <div className="flex justify-center">
+              <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 mr-2 rounded">
+                Button 1
+              </button>
+              <button className="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded">
+                Button 2
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="fixed top-0 left-1/2 transform -translate-x-1/2 z-50 w-full flex justify-center">
         {showToast && (
           <div className="toast toast-top toast-center">
